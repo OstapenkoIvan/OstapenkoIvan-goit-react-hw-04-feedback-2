343fb2adcd6bd59c4b962c6e40fbd48273a98028
OstapenkoIvan <ostapenko.ivan@gmail.com>
2022-10-07 22:48:12 +0300

    upd

--- src/components/title/index.js
+++ src/components/title/index.js
@@ -1,17 +1,1 @@
-import PropTypes from 'prop-types';
-import React from 'react';
-import s from './title.module.css';
-
-export const Title = ({ title, children }) => {
-  return (
-    <section className={s.container}>
-      <h2>{title}</h2>
-      {children}
-    </section>
-  );
-};
-
-Title.propTypes = {
-  title: PropTypes.string.isRequired,
-  children: PropTypes.element.isRequired,
-};
+export { Title } from './Title';
