e6901d77956d046ce0bca694691674bb7bc436fa
OstapenkoIvan <ostapenko.ivan@gmail.com>
2022-10-07 22:51:27 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import Feedback from './Feedback';
+import Feedback from './Feedback/Feedback';
 import { Title } from './Title';
 import Statistics from './Statistics';
 
